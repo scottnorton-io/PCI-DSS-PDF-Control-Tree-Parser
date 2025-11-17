e777e13b6c9401846985a02ab52890e654f9da4c
scott norton <81666759+scottnorton-io@users.noreply.github.com>
2025-11-17 08:14:51 -0700

    Enhance rebuild script with package install and parsing
    Add installation of local package and PCI-DSS parsing command

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,9 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+pip install .
+
+pci-dss-parse PCI-DSS-v4_0_1.pdf -f yaml -o controls.yaml
 
 # Run tests
 pytest tests/test_parser_basic.py
